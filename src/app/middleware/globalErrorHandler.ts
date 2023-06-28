ac936aceff9bcebbac9c20fd49cdd950727a5cac
Mostafizur-pro <mostafizur0195@gmail.com>
2023-06-28 08:11:34 +0600

    Global Error Handle add

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,23 +1,64 @@
-import express, { NextFunction, Request, Response } from 'express'
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import config from '../../config'
+import handleValidationError from '../../error/handleValidationError'
+import { ZodError } from 'zod'
+import handleZodError from '../../error/handleZodError'
+import ApiError from '../../error/ApiError'
 
 // Global error handler
-const globalErrorHandler = (
-  err,
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  config.env === 'development'
+    ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
+    : console.log(`🐱‍🏍 globalErrorHandler ~~`, error)
+
   let statusCode = 500
-  let message = 'Something went wrong'
-  let errorMessage: IGenericErrorMessage[] = []
+  let message = 'Something went wrong !'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
-    errorMessage,
-    stack: config.env !== 'production' ? err?.stack : undefined,
+    errorMessages,
+    stack: config.env !== 'production' ? error?.stack : undefined,
   })
 
   next()
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const createUserZodSchema = z.object({
   body: z.object({
     role: z.string(),
+    password: z.string().optional(),
   }),
 })
 
